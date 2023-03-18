b0cdbe9be5c41b53bd9129faab17f2c446e30683
Pete Navarra <vapokrocks@gmail.com>
2023-03-18 01:43:20 -0400

    Updating Version

--- AdventureBackpacks/AdventureBackpacks.cs
+++ AdventureBackpacks/AdventureBackpacks.cs
@@ -29,7 +29,7 @@ namespace AdventureBackpacks
         //Module Constants
         private const string _pluginId = "vapok.mods.adventurebackpacks";
         private const string _displayName = "Adventure Backpacks";
-        private const string _version = "1.6.12";
+        private const string _version = "1.6.13";
         
         //Interface Properties
         public string PluginId => _pluginId;
