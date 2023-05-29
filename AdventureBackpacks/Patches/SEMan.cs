22fffc3fcaa87c434b8b3bcbb9995ae4349f3c9c
Pete Navarra <vapokrocks@gmail.com>
2023-05-29 02:29:27 -0400

    Updates to SEMan

--- AdventureBackpacks/Patches/SEMan.cs
+++ AdventureBackpacks/Patches/SEMan.cs
@@ -7,17 +7,17 @@ namespace AdventureBackpacks.Patches;
 
 public static class SEManPatches
 {
-    [HarmonyPatch(typeof(SEMan), nameof(SEMan.RemoveStatusEffect), new[] { typeof(string), typeof(bool) })]
+    [HarmonyPatch(typeof(SEMan), nameof(SEMan.RemoveStatusEffect), new[] { typeof(int), typeof(bool) })]
     public static class RemoveStatusEffects
     {
         [UsedImplicitly]
         [HarmonyPriority(Priority.First)]
-        public static bool Prefix(string name, ref bool __result)
+        public static bool Prefix(int nameHash, ref bool __result)
         {
             if (EquipmentEffectCache.ActiveEffects == null)
                 return true;
         
-            if (EquipmentEffectCache.ActiveEffects.Any(x => x.name.Equals(name)))
+            if (EquipmentEffectCache.ActiveEffects.Any(x => x.NameHash().Equals(nameHash)))
             {
                 __result = false;
                 return false;
